181a758960824a9bd2d507229c6a7ffd207e6a2c
unknown <MLowe4@DEV.LOCAL>
2017-09-22 16:10:42 +0100

    Dragon fight

--- TextBasedAdventureGameSlashInteractiveFiction/TextBasedAdventureGameSlashInteractiveFiction.cpp
+++ TextBasedAdventureGameSlashInteractiveFiction/TextBasedAdventureGameSlashInteractiveFiction.cpp
@@ -15,6 +15,7 @@ const int kMapSize = 10;
 
 struct Item {
 	int hp, damage;
+	string name;
 };
 
 map<string, Item> items;
@@ -29,6 +30,14 @@ class Living {
 	public:
 		int hp;
 		int damage;
+};
+
+class Dragon : public Living {
+	public:
+		Dragon() {
+			hp = 40;
+			damage = 6;
+		}
 };
 
 /* Player class */
@@ -119,7 +128,11 @@ class GameMap {
 		}
 
 		Tile getTile(Coord pos) {
-			return map[pos.x][pos.y];
+			return map[pos.y][pos.x];
+		}
+
+		void updateTile(Coord pos, Tile t) {
+			map[pos.y][pos.x] = t;
 		}
 
 	private:
@@ -158,7 +171,7 @@ Coord moveDirection() {
 	return pos;
 }
 
-void pickupItem(shared_ptr<Player> player) {
+void pickupItem(shared_ptr<GameMap> gameMap, shared_ptr<Player> player) {
 	int prob = rand() % 10;
 	Item item;
 
@@ -168,12 +181,68 @@ void pickupItem(shared_ptr<Player> player) {
 		item = items["Chainmail"];
 	else
 		item = items["Dildo"];
+
+	cout << "You picked up a " << item.name << endl;
+
+	player->addHP(item.hp);
+	player->addDamage(item.damage);
 }
 
-void checkTile(shared_ptr<Player> player, GameMap::Tile tile) {
-	switch(tile) {
-		case GameMap::Tile::Item: pickupItem(player); break;
+void dragonFight(shared_ptr<Player> player) {
+	Dragon dragon;
+
+	while (dragon.hp > 0 && player->hp > 0) {
+		int damage = (rand() % 20) - 10 + player->damage;
+		dragon.hp -= damage;
+		cout << "You hit the dragon for " << damage << " damage.\n";
+		cout << "The dragon has " << dragon.hp << " hp.\n\n";
+
+		if (dragon.hp > 0) {
+			damage = (rand() % 6) + dragon.damage;
+			player->hp -= damage;
+			cout << "The dragon hit you for " << damage << " damage.\n";
+			cout << "you have " << player->hp << " hp.\n\n";
+		}
+		
+		system("pause");
+		system("cls");
 	}
+
+	if (dragon.hp < 0)
+		cout << "You defeated the dragon\n";
+	else
+		cout << "The dragon defeated you\n";
+
+	
+}
+
+void Riddles() {
+
+}
+
+bool checkTile(shared_ptr<GameMap> gameMap, shared_ptr<Player> player, Coord pos, GameMap::Tile tile) {
+	switch(tile) {
+		case GameMap::Tile::Item:
+			cout << "You have landed on an item!" << endl;
+			pickupItem(gameMap, player);
+			gameMap->updateTile(pos, GameMap::Tile::Empty);
+			cout << endl;
+			break;
+
+		case GameMap::Tile::Wall:
+			return false;
+			break;
+
+		case GameMap::Tile::Dragon:
+			dragonFight(player);
+			break;
+
+		case GameMap::Tile::Riddle:
+
+			break;
+	}
+
+	return true;
 }
 
 void initItems();
@@ -185,27 +254,28 @@ int main() {
 
 	initItems();
 
-	GameMap gameMap(kMapFile);
+	shared_ptr<GameMap> gameMap = make_shared<GameMap>(kMapFile);
 	shared_ptr<Player> player = make_shared<Player>();
 
 	while (true) {
 		player->outputStats();
 		cout << "\n\n";
 
-		gameMap.display();
+		gameMap->display();
 		cout << endl;
 
+		Coord playerPos = gameMap->getPlayerPosition();
 		Coord nPos = moveDirection();
-		Coord playerPos = gameMap.getPlayerPosition();
+
+		system("cls");
 
 		nPos.x += playerPos.x;
 		nPos.y += playerPos.y;
 
-		gameMap.updatePlayerPosition(nPos);
-
-		checkTile(player, gameMap.getTile(nPos));
-
-		system("cls");
+		if (checkTile(gameMap, player, nPos, gameMap->getTile(nPos)))
+			gameMap->updatePlayerPosition(nPos);
+		else
+			cout << "You can't move that way!\n";
 	}
 	
 	cout << "\n\n";
@@ -215,7 +285,7 @@ int main() {
 }
 
 void initItems() {
-	items["Sword"] = { 0, 10 };
-	items["Chainmail"] = { 50, 0 };
-	items["Dildo"] = { 0, 100 };
+	items["Sword"] = { 0, 10, "Sword" };
+	items["Chainmail"] = { 50, 0, "Chainmail" };
+	items["Dildo"] = { 0, 100, "Dildo" };
 }
